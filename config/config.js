1afd4debca676c69ce721c912615b8c5b53fec15
Mustafa <218740897+0x99M@users.noreply.github.com>
2025-08-14 07:17:26 +0300

    Use js config file instead of json file

---
+++ config/config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    dialect: 'postgres'
+  }
+};
